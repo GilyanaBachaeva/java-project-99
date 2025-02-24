589f55907e76fd02c3bdd495146313f8bb092e2a
Gilyana Bachaeva <gelchik29@icloud.com>
2025-02-24 21:38:33 +0300

    Update AppApplication.java

--- app/src/main/java/hexlet/code/demo/AppApplication.java
+++ app/src/main/java/hexlet/code/demo/AppApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		// Получение переменных окружения
+		String dbHost = System.getenv("DB_HOST");
+		String dbPort = System.getenv("DB_PORT");
+		String dbName = System.getenv("DB_NAME");
+		String dbUser = System.getenv("DB_USER");
+		String dbPassword = System.getenv("DB_PASSWORD");
 
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		// Установка свойств для подключения к базе данных
+		System.setProperty("DB_HOST", dbHost);
+		System.setProperty("DB_PORT", dbPort);
+		System.setProperty("DB_NAME", dbName);
+		System.setProperty("DB_USER", dbUser);
+		System.setProperty("DB_PASSWORD", dbPassword);
 
 		SpringApplication.run(AppApplication.class, args);
 	}
