96089014601a8be9292544abaaa4a7020b7e2d58
Gilyana Bachaeva <gelchik29@icloud.com>
2025-02-24 17:46:19 +0300

    First commit

---
+++ demo/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+	java
+	jacoco
+	id("org.springframework.boot") version "3.5.0-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
